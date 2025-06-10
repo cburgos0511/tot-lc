7ccb2426866795479f9b7f7490b44d6a7e97ff4e
Leoclimbes <leoclimbes2@gmail.com>
2025-06-10 11:52:22 -0500

    Characters Api
    I created the characters Api

--- server/models/character.js
+++ server/models/character.js
@@ -1,2 +1,107 @@
 // Character Model
 // TODO: Define character schema/model 
+
+export const CharacterTypes = ["Jedi,", "Smuggler", "Wookie", "Droid", "Princess", "Jedi Master", "Mandolorian", "Bounty Hunter"];
+
+export const characters = [
+
+{
+  name: "Luke Skywalker",
+  type: "Jedi",
+  strength: "Speed",
+  Weapons: "",
+  specialWeapon: "Light Saber",
+  power: "0.6",
+  forcePower: "O.9",
+  stealth: "0.8",
+  healingEfficincy: "0.7"
+},
+  {
+  name: "Han Solo",
+  type: "Smuggler",
+  strength: "Accuracy",
+  Weapons: "",
+  specialWeapon: "DL-44 Blaster",
+  power: "0.7",
+  forcePower: "0",
+  stealth: "0.6",
+  healingEfficincy: "0.6"
+  }, 
+  {
+  name: "Chewbacca",
+  type: "Wookie",
+  strength: "Strngth",
+  Weapons: "",
+  specialWeapon: "Bowcaster",
+  power: "0.9",
+  forcePower: "0",
+  stealth: "0.35",
+  healingEfficincy: "0.6"
+  },
+  {
+    name: "Princess Leia",
+    type: "Princess",
+    strength: "Negotaitors Edge",
+    Weapons: "",
+    specialWeapon: "Satines Lament",
+    power: "0.3",
+    forcePower: "0.2",
+    stealth: "0.85",
+    healingEfficincy: "0.6"
+    },
+    {
+    name: "R2-D2",
+    type: "Droid",
+    strength: "Shield",
+    Weapons: "",
+    specialWeapon: "Electro Pod",
+    power: "0.2",
+    forcePower: "0",
+    stealth: "0.5",
+    healingEfficincy: "0.6"
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        type: "Jedi Master",
+        strength: "Light Saber Combat", 
+        Weapons: "",
+        specialWeapon: "LightSaber",
+        power: "0.8",
+        forcePower: "0.8",
+        stealth: "0.7",
+        healingEfficincy: "0.7",  
+        }, 
+        {
+        name: "Yoda", 
+        type: "Jedi Master",
+        strength: "Force",
+        Weapons: "",
+        specialWeapon: "LightSaber",
+        power: "0.8",
+        forcePower: "1",
+        stealth: "0.9",
+        healingEfficincy: "0.9"
+        },
+        {
+            name: "Mandolorian",
+            type: "Mandolorian",
+            strength: "Armor",
+            Weapons: "",
+            specialWeapon: "Beskar Bullets",
+            power: "0.8",
+            forcePower: "0",
+            stealth: "0.5",
+            healingEfficincy: "0.4"
+        },
+        {
+            name: "Boba Fett",
+            type: "Bounty Hunter",
+            strength: "Accuracy",
+            Weapons: "",
+            specialWeapon: "Rocket",
+            power: "0.7",
+            forcePower: "0",
+            stealth: "0.5",
+            healingEfficincy: "0.5"
+        },
+];
